30fc9d6a32babdef9da59729ca184616cbb5ca28
Liz Johnson <liz.jade.johnson@gmail.com>
2024-04-18 13:52:23 -0400

    fixed typo

--- views/recipeDetail.jsx
+++ views/recipeDetail.jsx
@@ -3,21 +3,6 @@ const React = require('react');
 class RecipeDetail extends React.Component {
     render() {
         const { recipe } = this.props;
-        // Check if the recipe or components is undefined before attempting to render them
-        if (!recipe || !recipe.components) {
-            return (
-                <html>
-                    <head>
-                        <title>Recipe Not Found</title>
-                    </head>
-                    <body>
-                        <h1>Recipe Not Found</h1>
-                        <p>The requested recipe could not be found or has no components listed.</p>
-                    </body>
-                </html>
-            );
-        }
-
         return (
             <html>
                 <head>
@@ -41,7 +26,6 @@ class RecipeDetail extends React.Component {
 }
 
 module.exports = RecipeDetail;
-
 
 /* Iterating Over Resources: Instead of attempting to directly display recipe.resources.name e.g., which is not valid,
 the updated code maps over recipe.resources and renders each item in a list.
